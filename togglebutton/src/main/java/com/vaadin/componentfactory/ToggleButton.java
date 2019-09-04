f387add753585aac2984c8be17168dbe1ba83c29
Onur Idrisoglu <onur@vaadin.com>
2019-09-04 13:49:19 +0300

    updated jsmodule path

--- togglebutton/src/main/java/com/vaadin/componentfactory/ToggleButton.java
+++ togglebutton/src/main/java/com/vaadin/componentfactory/ToggleButton.java
@@ -39,7 +39,7 @@ import com.vaadin.flow.shared.Registration;
 
 @Tag("vcf-toggle-button")
 @NpmPackage(value = "@vaadin-component-factory/vcf-toggle-button", version = "1.0.1")
-@JsModule("@vaadin-component-factory/vcf-toggle-button/src/vcf-toggle-button.js")
+@JsModule("@vaadin-component-factory/vcf-toggle-button")
 @SuppressWarnings("serial")
 public class ToggleButton extends
         AbstractSinglePropertyField<ToggleButton, Boolean> implements HasStyle,
